4d3b48ce5edc46837578892acb6807a7d9860a30
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-01-30 23:35:58 -0300

    Add files via upload

--- qualidade_educacao_democracia_autocracia.R
+++ qualidade_educacao_democracia_autocracia.R
@@ -152,6 +152,8 @@ pisa_sci3 <- pisa_sci %>%
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
 
+### Leitura
+
 c4a("safe", 4)
 
 ggplot(pisa_read1, aes(x = fct_reorder(Entity, media), 
@@ -193,6 +195,55 @@ ggplot(pisa_read3, aes(x = factor(Year), y = performance,
   scale_x_discrete(expand = expansion(mult = c(0.1,0.1))) +
   labs(x = "Tempo (anos)", 
        y = "Desempenho em leitura \nPISA test score",
+       color = "Países") +
+  theme_light() +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(color = "black", size = 15),
+        legend.text = element_text(size = 12))
+
+### Matemática
+
+c4a("safe", 4)
+
+ggplot(pisa_math1, aes(x = fct_reorder(Entity, media), 
+                       y = media, fill = Entity)) +
+  geom_col(width = 0.9) +
+  geom_errorbar(aes(ymin = media - se, ymax = media + se),
+                width = 0.2, size = 0.8) +
+  scale_fill_manual(values = c("#88CCEE", "#CC6677",
+                               "#DDCC77", "#117733")) +
+  scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  scale_x_discrete(labels = c("Estados \nUnidos", "Alemanha",
+                              "Japão", "China")) +
+  labs(x = "Países", y = "Desempenho em matemática \nPISA test score") +
+  theme_ipsum(axis_text_size = 14, axis_title_size = 16) +
+  theme(legend.position = "none",
+        axis.text = element_text(color = "black"))
+
+ggplot(pisa_math2, aes(x = factor(Year), y = performance,
+                       color = Entity, group = Entity)) +
+  geom_point(shape = 15, size = 2.5) +
+  geom_line(size = 1.2) +
+  scale_color_manual(values = c("#88CCEE", "#CC6677",
+                                "#DDCC77", "#117733"),
+                     labels = c("China", "Alemanha",
+                                "Japão", "Estados Unidos")) +
+  scale_x_discrete(expand = expansion(mult = c(0.1,0.1))) +
+  labs(x = "Tempo (anos)", 
+       y = "Desempenho em matemática \nPISA test score",
+       color = "Países") +
+  theme_ipsum(axis_text_size = 14, 
+              axis_title_size = 16) +
+  theme(axis.text = element_text(color = "black"))
+                                 
+ggplot(pisa_math3, aes(x = factor(Year), y = performance,
+                       color = Entity, group = Entity)) +
+  geom_line(size = 2) +
+  scale_color_manual(values = c('#1B9E77', '#999999','#E69F00'),
+                     labels = c("Brasil", "China", "Estados Unidos")) +
+  scale_x_discrete(expand = expansion(mult = c(0.1,0.1))) +
+  labs(x = "Tempo (anos)", 
+       y = "Desempenho em matemática \nPISA test score",
        color = "Países") +
   theme_light() +
   theme(axis.title = element_text(size = 18),
